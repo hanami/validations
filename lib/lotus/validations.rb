298978a7f8860a49c3897eebf048e54793ec1c2b
Luca Guidi <me@lucaguidi.com>
2014-08-03 17:29:55 +0200

    Initial mess

---
+++ lib/lotus/validations.rb
@@ -0,0 +1,7 @@
+require "lotus/validations/version"
+
+module Lotus
+  module Validations
+    # Your code goes here...
+  end
+end
---
+++ lib/lotus/validations/version.rb
@@ -0,0 +1,5 @@
+module Lotus
+  module Validations
+    VERSION = "0.0.1"
+  end
+end
