621dec3afe321b528dc319f8544fd92d759215a4
Luca Guidi <me@lucaguidi.com>
2014-08-06 12:07:26 +0200

    Forgot to commit an example

--- test/acceptance_validation_test.rb
+++ test/acceptance_validation_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 describe Lotus::Validations do
   describe 'acceptance' do
-    # it "is valid if it doesn't have attributes" do
-    #   validator = AcceptanceValidatorTest.new({})
+    it "is isn't valid if it doesn't have attributes" do
+      validator = AcceptanceValidatorTest.new({})
 
-    #   validator.valid?.must_equal true
-    #   validator.errors.must_be_empty
-    # end
+      validator.valid?.must_equal false
+      validator.errors.fetch(:tos).must_include :acceptance
+    end
 
     it "is valid if it the value can be coercible to TrueClass" do
       [1, '1', true, Object.new, :thruthy].each do |value|
