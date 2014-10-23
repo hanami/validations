437f00947d9f7eee47e42384f5f5bfc4e6f83f05
Luca Guidi <me@lucaguidi.com>
2014-10-23 13:03:46 +0200

    Prepare for v0.1.0

--- lib/lotus/validations/version.rb
+++ lib/lotus/validations/version.rb
@@ -1,6 +1,6 @@
 module Lotus
   module Validations
     # @since 0.1.0
-    VERSION = "0.0.1"
+    VERSION = '0.1.0'.freeze
   end
 end
---
+++ test/version_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+describe Lotus::Validations::VERSION do
+  it 'exposes version' do
+    Lotus::Validations::VERSION.must_equal '0.1.0'
+  end
+end
