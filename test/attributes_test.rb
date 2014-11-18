e6a1bcae35ea62fd09ae520fd8a5301dccbce9ab
Luca Guidi <me@lucaguidi.com>
2014-11-18 14:06:57 +0100

    Implement Validations#each and remove #attributes

--- test/attributes_test.rb
+++ test/attributes_test.rb
@@ -5,26 +5,26 @@ describe Lotus::Validations do
     @validator = VisibilityValidatorTest.new(name: 'Luca', unwanted: 'nope', password: 'secret', password_confirmation: 'secret')
   end
 
-  describe '#attributes' do
-    it 'makes the set of attributes available' do
-      @validator.attributes.must_equal({name: 'Luca', password: 'secret', password_confirmation: 'secret'})
-    end
-
-    it 'prevents information escape' do
-      @validator.attributes[:name].reverse!
-
-      @validator.attributes.must_equal({name: 'Luca', password: 'secret', password_confirmation: 'secret'})
-    end
-  end
-
   describe '#to_h' do
-    it 'makes the set of attributes available' do
+    it 'returns a Hash of the internal attributes' do
       @validator.to_h.must_equal({name: 'Luca', password: 'secret', password_confirmation: 'secret'})
     end
 
     it 'prevents information escape' do
       @validator.to_h[:name].reverse!
       @validator.to_h.must_equal({name: 'Luca', password: 'secret', password_confirmation: 'secret'})
+    end
+  end
+
+  describe '#each' do
+    it 'iterates thru the given block' do
+      result = {}
+
+      @validator.each do |attr, value|
+        result[attr] = value
+      end
+
+      result.must_equal(@validator.to_h)
     end
   end
 end
