ebe2271913ce0245afc1d5d808e1a8ea11e6cb24
Luca Guidi <me@lucaguidi.com>
2014-11-22 16:35:46 +0100

    Bump version to v0.2.0

--- lib/lotus/validations/version.rb
+++ lib/lotus/validations/version.rb
@@ -1,6 +1,6 @@
 module Lotus
   module Validations
     # @since 0.1.0
-    VERSION = '0.2.0.dev'.freeze
+    VERSION = '0.2.0'.freeze
   end
 end
--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Lotus::Validations::VERSION do
   it 'exposes version' do
-    Lotus::Validations::VERSION.must_equal '0.2.0.dev'
+    Lotus::Validations::VERSION.must_equal '0.2.0'
   end
 end
