9ced88990805b3d9aa1f160a9701e9f3c4d8702f
Roberto Ciatti <roberto.ciatti@overhilllab.com>
2014-11-27 12:29:17 +0100

    Minitest style specs

--- test/attribute_accessor_test.rb
+++ test/attribute_accessor_test.rb
@@ -8,19 +8,19 @@ describe Lotus::Validations do
 
     it 'test attribute write possibility' do
       obj = AttributeAccessorTest.new attr: 5
-      assert_equal 5, obj.attr
+      obj.attr.must_equal 5
 
       obj.attr = 6
-      assert_equal 6, obj.attr
+      obj.attr.must_equal 6
     end
 
     it 'test attribute valid and writable' do
       obj = WritablePresenceValidatorTest.new name: "Roberto"
       assert obj.valid?
-      assert_equal "Roberto", obj.name
+      obj.name.must_equal "Roberto"
 
       obj.name = "Matteo"
-      assert_equal "Matteo", obj.name
+      obj.name.must_equal "Matteo"
     end
 
   end
