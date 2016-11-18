b75ccc87e4588b58c9fd5bd61dd242199dcfe177
Luca Guidi <me@lucaguidi.com>
2016-11-18 16:04:13 +0100

    Prepare for v0.7.1

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Hanami::Validations::VERSION do
   it 'exposes version' do
-    Hanami::Validations::VERSION.must_equal '0.7.0'
+    Hanami::Validations::VERSION.must_equal '0.7.1'
   end
 end
