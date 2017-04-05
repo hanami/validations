cb406a2d725b593706c0f36aaa106e7869904eab
Luca Guidi <me@lucaguidi.com>
2017-04-05 10:33:16 +0200

    Prepare for v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 # Hanami::Validations
 Validations mixin for Ruby objects
 
+## v1.0.0 - 2017-04-06
+
 ## v1.0.0.rc1 - 2017-03-31
 
 ## v1.0.0.beta2 - 2017-03-17
--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Hanami::Validations::VERSION do
   it 'exposes version' do
-    Hanami::Validations::VERSION.must_equal '1.0.0.rc1'
+    Hanami::Validations::VERSION.must_equal '1.0.0'
   end
 end
