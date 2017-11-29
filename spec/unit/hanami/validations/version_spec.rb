1bc82603b091b741942cf2e0354062db957102f0
Luca Guidi <me@lucaguidi.com>
2017-11-29 13:47:40 +0100

    Bump version to v2.0.0.alpha1

--- spec/unit/hanami/validations/version_spec.rb
+++ spec/unit/hanami/validations/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Validations::VERSION" do
   it "exposes version" do
-    expect(Hanami::Validations::VERSION).to eq("1.1.0")
+    expect(Hanami::Validations::VERSION).to eq("2.0.0.alpha1")
   end
 end
