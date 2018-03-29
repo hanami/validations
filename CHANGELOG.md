c838281a2521f9065e187ee83fd2946223e04063
Luca Guidi <me@lucaguidi.com>
2018-03-29 14:58:15 +0200

    Prepare for v1.2.0.rc1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 # Hanami::Validations
 Validations mixin for Ruby objects
 
+## v1.2.0.rc1 - 2018-03-30
+
 ## v1.2.0.beta2 - 2018-03-23
 
 ## v1.2.0.beta1 - 2018-02-28
--- spec/unit/hanami/validations/version_spec.rb
+++ spec/unit/hanami/validations/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Validations::VERSION" do
   it "exposes version" do
-    expect(Hanami::Validations::VERSION).to eq("1.2.0.beta2")
+    expect(Hanami::Validations::VERSION).to eq("1.2.0.rc1")
   end
 end
