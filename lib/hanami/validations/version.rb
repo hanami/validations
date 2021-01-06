5c2a7610364ab0d613a9d81310a271f09615af70
Luca Guidi <me@lucaguidi.com>
2021-01-06 11:10:34 +0100

    Prepare for v1.3.7

--- lib/hanami/validations/version.rb
+++ lib/hanami/validations/version.rb
@@ -3,6 +3,6 @@
 module Hanami
   module Validations
     # @since 0.1.0
-    VERSION = "1.3.6"
+    VERSION = "1.3.7"
   end
 end
--- spec/unit/hanami/validations/version_spec.rb
+++ spec/unit/hanami/validations/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Validations::VERSION" do
   it "exposes version" do
-    expect(Hanami::Validations::VERSION).to eq("1.3.6")
+    expect(Hanami::Validations::VERSION).to eq("1.3.7")
   end
 end
