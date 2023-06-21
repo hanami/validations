4e191ba45b9172b3203133e0502df49a60d183d6
Tim Riley <tim@riley.id.au>
2023-06-21 17:04:59 +1000

    Bump version to 2.1.0.beta1

--- spec/unit/hanami/validations/version_spec.rb
+++ spec/unit/hanami/validations/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Validations::VERSION" do
   it "returns current version" do
-    expect(Hanami::Validations::VERSION).to eq("2.0.1")
+    expect(Hanami::Validations::VERSION).to eq("2.1.0.beta1")
   end
 end
