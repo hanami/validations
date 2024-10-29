ad2765301efdb99e43b842b2d51c4101bbfcd8ec
Tim Riley <tim@riley.id.au>
2024-10-29 17:33:43 +1100

    Prepare for v2.2.0.rc1

--- spec/unit/hanami/validations/version_spec.rb
+++ spec/unit/hanami/validations/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Validations::VERSION" do
   it "returns current version" do
-    expect(Hanami::Validations::VERSION).to eq("2.2.0.beta2")
+    expect(Hanami::Validations::VERSION).to eq("2.2.0.rc1")
   end
 end
